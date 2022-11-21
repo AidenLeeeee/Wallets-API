788035327177f88420ce604b85134639e3dbc2df
AidenLeeeee <hoo8799@gmail.com>
2022-11-21 16:30:02 +0900

    Refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WalletsModule } from './wallets/wallets.module';
 import { TradeLogsModule } from './trade-logs/trade-logs.module';
@@ -30,7 +28,5 @@ import { HistoriesModule } from './histories/histories.module';
     TradeLogsModule,
     HistoriesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
